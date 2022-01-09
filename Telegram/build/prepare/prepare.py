95276abfa2aa11e5392f6d59c989ba86dc0ffd66
ivanov <72218009+ivanovivan12589@users.noreply.github.com>
2022-01-09 18:51:03 +0100

    Increase version, add logging error in translate, remove debug for building

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -441,7 +441,6 @@ stage('lzma', """
 win:
     git clone https://github.com/desktop-app/lzma.git
     cd lzma\\C\\Util\\LzmaLib
-debug:
     msbuild LzmaLib.sln /property:Configuration=Debug /property:Platform="$X8664"
 release:
     msbuild LzmaLib.sln /property:Configuration=Release /property:Platform="$X8664"
@@ -586,7 +585,6 @@ stage('rnnoise', """
     cd out
 win:
     cmake -A %WIN32X64% ..
-debug:
     cmake --build . --config Debug
 release:
     cmake --build . --config Release
@@ -895,7 +893,6 @@ win:
         -A %WIN32X64% ^
         -D LIBTYPE:STRING=STATIC ^
         -D FORCE_STATIC_VCRT=ON
-debug:
     msbuild OpenAL.vcxproj /property:Configuration=Debug /property:Platform="%WIN32X64%"
 release:
     msbuild OpenAL.vcxproj /property:Configuration=RelWithDebInfo /property:Platform="%WIN32X64%"
@@ -944,7 +941,6 @@ win:
     cd src\\client\\windows
     gyp --no-circular-check breakpad_client.gyp --format=ninja
     cd ..\\..
-debug:
     ninja -C out/Debug%FolderPostfix% common crash_generation_client exception_handler
 release:
     ninja -C out/Release%FolderPostfix% common crash_generation_client exception_handler
@@ -958,7 +954,6 @@ mac:
     git checkout e1e7b0ad8e
     cd ../../..
     cd src/client/mac
-debug:
     xcodebuild -project Breakpad.xcodeproj -target Breakpad -configuration Debug build
 release:
     xcodebuild -project Breakpad.xcodeproj -target Breakpad -configuration Release build
@@ -977,7 +972,6 @@ mac:
     ZLIB_LIB=$USED_PREFIX/lib/libz.a
     mkdir out
     cd out
-debug:
     mkdir Debug.x86_64
     cd Debug.x86_64
     cmake -G Ninja \
@@ -1066,7 +1060,6 @@ depends:patches/qtbase_5_15_2/*.patch
 win:
     for /r %%i in (..\\..\\patches\\qtbase_5_15_2\\*) do git apply %%i
     cd ..
-debug:
     SET CONFIGURATIONS=-debug
 release:
     SET CONFIGURATIONS=-debug-and-release
@@ -1111,7 +1104,6 @@ win:
 mac:
     find ../../patches/qtbase_5_15_2 -type f -print0 | sort -z | xargs -0 git apply
     cd ..
-debug:
     CONFIGURATIONS=-debug
 release:
     CONFIGURATIONS=-debug-and-release
@@ -1153,7 +1145,6 @@ depends:patches/qt5compat_6_2_2/*.patch
 
     find ../../patches/qt5compat_6_2_2 -type f -print0 | sort -z | xargs -0 git apply
     cd ..
-debug:
     CONFIGURATIONS=-debug
 release:
     CONFIGURATIONS=-debug-and-release
@@ -1189,7 +1180,6 @@ win:
     SET FFMPEG_PATH=$LIBS_DIR/ffmpeg
     mkdir out
     cd out
-debug:
     mkdir Debug
     cd Debug
     cmake -G Ninja \
@@ -1220,7 +1210,6 @@ mac:
     FFMPEG_PATH=$USED_PREFIX/include
     mkdir out
     cd out
-debug:
     mkdir Debug.x86_64
     cd Debug.x86_64
     cmake -G Ninja \
