477728b15420340895be38194b6f959a24ce00e3
Kolya <142352140+agl-1984@users.noreply.github.com>
2024-02-08 18:59:16 +0100

    add release scripts

---
+++ Telegram/build/release/config.py
@@ -0,0 +1,8 @@
+
+# TG APP_ID and APP_HADH
+# https://my.telegram.org/auth?to=myapp
+API_ID = <INSERT_HERE>
+API_HASH = '<INSERT_HERE>'
+
+# https://github.com/settings/tokens
+GH_TOKEN = "github_pat_<INSERT_HERE>"
