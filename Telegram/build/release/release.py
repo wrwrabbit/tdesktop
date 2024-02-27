d7f838332ed178db296e0a73908ccd0e1253a620
Kolya <142352140+agl-1984@users.noreply.github.com>
2024-02-27 21:21:28 +0100

    allow upload several files at a time

--- Telegram/build/release/release.py
+++ Telegram/build/release/release.py
@@ -272,7 +272,11 @@ def do_list(args):
 def do_upload(args):
     # check id?
     # unpack file!
-    newfiles = get_file_data(args.ids[0])
+    newfiles = []
+    for fid in args.ids:
+        newfiles_ = get_file_data(fid)
+        if newfiles_:
+            newfiles += newfiles_
     if not newfiles:
         print("No files to upload")
         sys.exit(1)
