dd8319110f49f2763b45d1d4fc85519be625ed18
Kolya <142352140+agl-1984@users.noreply.github.com>
2025-07-31 23:25:44 +0300

    v1.9.9

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -28,6 +28,6 @@ constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
 
 constexpr auto FakeAppVersion = 0100000;
-constexpr auto PTelegramAppVersion = 1009008;
-constexpr auto PTelegramAppVersionStr = "1.9.8";
+constexpr auto PTelegramAppVersion = 1009009;
+constexpr auto PTelegramAppVersionStr = "1.9.9";
 constexpr auto PTelegramAppBetaVersion = false;
