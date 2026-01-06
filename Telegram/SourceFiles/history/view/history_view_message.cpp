5d9523466b6048829cff41eacc8403f666028805
23rd <23rd@vivaldi.net>
2026-01-06 18:38:53 +0300

    Fixed optimized repaint of message text while swipe gesture.
    Related commits: 8638d26156 and 9027e3b7ee.

--- Telegram/SourceFiles/history/view/history_view_message.cpp
+++ Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -1902,7 +1902,8 @@ void Message::paintText(
 		trect.setY(trect.y() + botTop->height);
 	}
 	if (!context.clip.intersects(trect)
-		&& context.skipDrawingParts == PaintContext::SkipDrawingParts::None) {
+		&& context.skipDrawingParts == PaintContext::SkipDrawingParts::None
+		&& !context.gestureHorizontal.translation) {
 		return;
 	}
 	prepareCustomEmojiPaint(p, context, text());
