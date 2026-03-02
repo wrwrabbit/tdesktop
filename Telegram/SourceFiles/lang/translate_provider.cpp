df1a8346e0fd940e4035ad10162391d2eb76eea8
23rd <23rd@vivaldi.net>
2026-03-02 17:53:01 +0300

    Added new translate provider that uses url template from experimental.

--- Telegram/SourceFiles/lang/translate_provider.cpp
+++ Telegram/SourceFiles/lang/translate_provider.cpp
@@ -7,6 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "lang/translate_provider.h"
 
+#include "base/options.h"
 #include "core/application.h"
 #include "core/core_settings.h"
 #include "data/data_msg_id.h"
@@ -14,12 +15,29 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_session.h"
 #include "history/history_item.h"
 #include "lang/translate_mtproto_provider.h"
+#include "lang/translate_url_provider.h"
 #include "platform/platform_translate_provider.h"
+
+namespace {
+
+base::options::option<QString> OptionTranslateUrlTemplate({
+	.id = "translate-url-template",
+	.name = "Translate URL template",
+	.description = "Template URL for custom translation provider."
+		" Supports %q text, %f source language and %t target language.",
+});
+
+} // namespace
 
 namespace Ui {
 
 std::unique_ptr<TranslateProvider> CreateTranslateProvider(
 		not_null<Main::Session*> session) {
+	const auto urlTemplate = OptionTranslateUrlTemplate.value();
+	if (!urlTemplate.isEmpty()
+		&& urlTemplate.contains(u"%q"_q)) {
+		return CreateUrlTranslateProvider(urlTemplate);
+	}
 	if (Core::App().settings().usePlatformTranslation()
 		&& Platform::IsTranslateProviderAvailable()) {
 		return Platform::CreateTranslateProvider();
