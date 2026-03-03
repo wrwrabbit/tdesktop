wrwrabbit/tdesktop

commit b2531b588ffaf5d8cee6046f48d654c8a15c12bf

Fixed possible crash from rection preview.

Telegram/SourceFiles/history/view/history_view_reaction_preview.cpp
@@ -53,8 +53,16 @@ bool ShowReactionPreview(
 		base::unique_qptr<Ui::AbstractButton> clickable;
 		base::unique_qptr<Ui::AbstractButton> background;
 		base::unique_qptr<Ui::FlatLabel> label;
+
+		void clear() {
+			mediaPreview.reset();
+			clickable.reset();
+			background.reset();
+			label.reset();
+		};
+
 	};
-	const auto state = std::make_shared<State>();
+	const auto state = std::make_shared<PrintDead<State>>();
 
 	const auto mainwidget = controller->widget();
 	state->mediaPreview = base::make_unique_q<Window::MediaPreviewWidget>(
@@ -73,9 +81,7 @@ bool ShowReactionPreview(
 		}
 		base::call_delayed(
 			st::defaultToggle.duration,
-			crl::guard(state->clickable.get(), [=] {
-				state->clickable.reset();
-			}));
+			[=] { state->clear(); });
 	};
 	state->clickable->setClickedCallback(hideAll);
 	base::install_event_filter(QCoreApplication::instance(), [=](
@@ -150,8 +156,6 @@ bool ShowReactionPreview(
 
 	mainwidget->sizeValue() | rpl::on_next([=](QSize size) {
 		mediaPreviewRaw->setGeometry(Rect(size));
-		clickableRaw->setGeometry(Rect(size));
-		clickableRaw->raise();
 
 		if (backgroundRaw && labelRaw) {
 			const auto maxLabelWidth = labelRaw->textMaxWidth() / 2;
@@ -174,6 +178,11 @@ bool ShowReactionPreview(
 			backgroundRaw->raise();
 		}
 	}, mediaPreviewRaw->lifetime());
+
+	mainwidget->sizeValue() | rpl::on_next([=](QSize size) {
+		clickableRaw->setGeometry(Rect(size));
+		clickableRaw->raise();
+	}, clickableRaw->lifetime());
 	return true;
 }
 

commit 3a7bf449ed9cb5b4ca10a49638d545de83354b91

Fixed ability to open emoji pack from rection preview.
Regression was introduced in b2531b588f.

Telegram/SourceFiles/history/view/history_view_reaction_preview.cpp
@@ -155,6 +155,11 @@ bool ShowReactionPreview(
 	const auto labelRaw = state->label.get();
 
 	mainwidget->sizeValue() | rpl::on_next([=](QSize size) {
+		clickableRaw->setGeometry(Rect(size));
+		clickableRaw->raise();
+	}, clickableRaw->lifetime());
+
+	mainwidget->sizeValue() | rpl::on_next([=](QSize size) {
 		mediaPreviewRaw->setGeometry(Rect(size));
 
 		if (backgroundRaw && labelRaw) {
@@ -178,11 +183,6 @@ bool ShowReactionPreview(
 			backgroundRaw->raise();
 		}
 	}, mediaPreviewRaw->lifetime());
-
-	mainwidget->sizeValue() | rpl::on_next([=](QSize size) {
-		clickableRaw->setGeometry(Rect(size));
-		clickableRaw->raise();
-	}, clickableRaw->lifetime());
 	return true;
 }
 