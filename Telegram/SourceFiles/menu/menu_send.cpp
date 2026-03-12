aa78825b7b2594077894fb4e1bdb0abefec31b78
John Preston <johnprestonmail@gmail.com>
2026-03-12 13:09:50 +0400

    Revert "Update menu_send.cpp"
    This reverts commit f9bf1e937233c4220d17d45e5710d9a8f52c326f.
    This reverts commit a08f8d890d51ac59d53dd0b10474eeb1ee466a99.
    
    Fair point raised on GitHub, that the cost of "click-by-memory" may
    be too high in this case over the consistency gains, with admins
    sending messages instantly instead of scheduling, like it did before.

--- Telegram/SourceFiles/menu/menu_send.cpp
+++ Telegram/SourceFiles/menu/menu_send.cpp
@@ -737,6 +737,13 @@ FillMenuResult FillSendMenu(
 	const auto &icons = iconsOverride
 		? *iconsOverride
 		: st::defaultComposeIcons;
+
+	if (sending && type != Type::Reminder) {
+		menu->addAction(
+			tr::lng_send_silent_message(tr::now),
+			[=] { action({ Api::SendOptions{ .silent = true } }, details); },
+			&icons.menuMute);
+	}
 	if (sending && type != Type::SilentOnly) {
 		menu->addAction(
 			((type == Type::Reminder)
@@ -753,13 +760,7 @@ FillMenuResult FillSendMenu(
 				details); },
 			&icons.menuWhenOnline);
 	}
-	if (sending && type != Type::Reminder) {
-		menu->addAction(
-			tr::lng_send_silent_message(tr::now),
-			[=] { action({ Api::SendOptions{ .silent = true } }, details); },
-			&icons.menuMute);
 
-	}
 	if ((type != Type::Disabled)
 		&& ((details.spoiler != SpoilerState::None)
 			|| (details.caption != CaptionState::None)
