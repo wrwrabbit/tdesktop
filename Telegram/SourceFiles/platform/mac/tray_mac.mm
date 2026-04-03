wrwrabbit/tdesktop

commit 8de878693df5f4dc6e918bb794000d56f4d79153

Fixed initial tray icon color on macOS dark theme.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,7 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = status.button.effectiveAppearance;
+	const auto appearance = [NSApp effectiveAppearance];
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 

commit 172bbb1b292b8376b65187676d201eda31c0357a

Fixed crash on macOS 10.13.
Regression was introduced in 8de878693d.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,12 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = [NSApp effectiveAppearance];
+	const auto appearance = [&] {
+		if (@available(macOS 10.14, *)) {
+			return [NSApp effectiveAppearance];
+		}
+		return status.button.effectiveAppearance;
+	}();
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 