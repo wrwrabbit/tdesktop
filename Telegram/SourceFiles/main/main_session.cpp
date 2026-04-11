8ed9085474a4560b46d94cb0a117937f8c831385
23rd <23rd@vivaldi.net>
2026-04-11 17:11:30 +0300

    Added recent search entries to settings.

--- Telegram/SourceFiles/main/main_session.cpp
+++ Telegram/SourceFiles/main/main_session.cpp
@@ -41,6 +41,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/components/sponsored_messages.h"
 #include "data/components/top_peers.h"
 #include "settings/settings_faq_suggestions.h"
+#include "settings/settings_recent_searches.h"
 #include "data/data_session.h"
 #include "data/data_changes.h"
 #include "data/data_user.h"
@@ -159,6 +160,7 @@ Session::Session(
 }))
 , _passkeys(std::make_unique<Data::Passkeys>(this))
 , _faqSuggestions(std::make_unique<Settings::FaqSuggestions>(this))
+, _recentSettingsSearches(std::make_unique<Settings::RecentSearches>(this))
 , _cachedReactionIconFactory(std::make_unique<ReactionIconFactory>())
 , _supportHelper(Support::Helper::Create(this))
 , _fastButtonsBots(std::make_unique<Support::FastButtonsBots>(this))
