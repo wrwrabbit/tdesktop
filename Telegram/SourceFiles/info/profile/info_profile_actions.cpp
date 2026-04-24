e0461b89fbf83f94fe95897df919c74381f3a7cc
23rd <23rd@vivaldi.net>
2026-04-24 12:07:32 +0300

    Slightly improved empty gap before block button in info profile.

--- Telegram/SourceFiles/info/profile/info_profile_actions.cpp
+++ Telegram/SourceFiles/info/profile/info_profile_actions.cpp
@@ -2842,11 +2842,7 @@ void ActionsFiller::fillUserActions(not_null<UserData*> user) {
 	if (!user->isSelf() && !user->isSupport() && !user->isVerifyCodes()) {
 		if (user->isBot()) {
 			addBotCommandActions(user);
-		}
-		_wrap->add(CreateSkipWidget(
-			_wrap,
-			st::infoBlockButtonSkip));
-		if (user->isBot()) {
+			_wrap->add(CreateSkipWidget(_wrap, st::infoBlockButtonSkip));
 			addReportAction();
 		}
 		addBlockAction(user);
