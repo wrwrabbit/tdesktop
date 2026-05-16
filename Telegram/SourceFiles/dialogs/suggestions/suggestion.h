36d13e1d6a18394fb852a058a587b2b9aaab356d
23rd <23rd@vivaldi.net>
2026-05-16 23:23:27 +0300

    Added custom promo spec to top bar suggestion table.

--- Telegram/SourceFiles/dialogs/suggestions/suggestion.h
+++ Telegram/SourceFiles/dialogs/suggestions/suggestion.h
@@ -60,6 +60,7 @@ struct Spec {
 
 [[nodiscard]] std::vector<Spec> AllSpecs();
 
+[[nodiscard]] Spec MakeCustomPromoSpec();
 [[nodiscard]] Spec MakeGiftAuctionsSpec();
 [[nodiscard]] Spec MakeUnreviewedAuthSpec();
 
