f8f77af67e5ed0ef59ec6415e21cb78ce1c66f82
23rd <23rd@vivaldi.net>
2026-05-16 23:13:22 +0300

    Added top bar suggestion spec types.

---
+++ Telegram/SourceFiles/dialogs/suggestions/suggestion.cpp
@@ -0,0 +1,16 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include "dialogs/suggestions/suggestion.h"
+
+namespace Dialogs::TopBarSuggestions {
+
+std::vector<Spec> AllSpecs() {
+	return {};
+}
+
+} // namespace Dialogs::TopBarSuggestions
