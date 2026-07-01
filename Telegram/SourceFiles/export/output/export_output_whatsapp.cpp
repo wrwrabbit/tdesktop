476f5fd2946150d76c45631148f11010ed5fc9d0
McOsterDonald <mcosterdonald@gmail.com>
2026-07-01 21:40:54 +0300

    delete comment

--- Telegram/SourceFiles/export/output/export_output_whatsapp.cpp
+++ Telegram/SourceFiles/export/output/export_output_whatsapp.cpp
@@ -1,10 +1,3 @@
-/*
-This file is part of Telegram Desktop,
-the official desktop application for the Telegram messaging service.
-
-For license and copyright information please follow this link:
-https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
-*/
 #include "export/output/export_output_whatsapp.h"
 
 #include "export/output/export_output_result.h"
